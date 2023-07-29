c2092c6c918e32b20229a1200aae3bc62d4b0f80
Finn <oeste.fin@hhg-schueler.de>
2023-07-29 14:41:20 +0200

    Footer Updates
    Design + Added links

--- js/templates.js
+++ js/templates.js
@@ -45,35 +45,28 @@ class Footer extends HTMLElement {
                 <div class="container">
                     <div class="row">
                         <div class="footer-col">
+                        <a href="https://www.google.com/maps/place/Kronenstra%C3%9Fe+43,+73734+Esslingen+am+Neckar/@48.7175723,9.3168188,17z/data=!3m1!4b1!4m6!3m5!1s0x4799c0e25be01beb:0xf561962797e44171!8m2!3d48.7175688!4d9.3193937!16s%2Fg%2F11bw431cbs?entry=ttu" target="_blank">
                             <h4>Anschrift</h4>
-                            <ul>
-                                <li>Praxis Dr. med. Sven Schönthal</li>
-                                <li>Facharzt für Innere Medizin (häusliche Versorgung)</li>
-                                <li>Kronenstr. 43</li>
-                                <li>73734 Esslingen am Neckar</li>
-                            </ul>
+                            <p><b>Praxis Dr. med. Sven Schönthal</b></p>
+                            <p>Facharzt für Innere Medizin (häusliche Versorgung)</p>
+                            <p>Kronenstr. 43</p>
+                            <p>73734 Esslingen am Neckar</p>
+                        </a>
                         </div>
                         <div class="footer-col">
                             <h4>Sprechzeiten</h4>
-                            <div style="border:1px solid black">
-                                <b>Montag - Mittwoch</b>
-                                <p>8:30-12:00 Uhr und 16:00-18:00 Uhr</p>
-                            </div>
-                            <div style="border:1px solid black">
-                                <b>Donnerstag - Freitag</b>
-                                <p>8:30-12:00 Uhr</p>
-                            </div>
+                            <p><span>Montag - Mittwoch</span><br>8:30-12:00 Uhr und 16:00-18:00 Uhr</p>
+                            <br>
+                            <p><span>Donnerstag - Freitag</span><br>8:30-12:00 Uhr</p>
                         </div>
                         <div class="footer-col">
                             <h4>Kontakt</h4>
-                            <table>
-                                <tr><th>Telefon:</th><td>0711 3451011</td></tr>
-                                <tr><th>Fax:</th><td>0711 52853896</td></tr>
-                                <tr><th>E-Mail:</th><td>dr.schoenthal@meinhausarzt.biz</td></tr>
-                                <tr><th>Rezeptbestellung:</th><td>rezeptservice@meinhausarzt.biz</td></tr>
-                                <tr><th>Überweisungsanfrage:</th><td>ueberweisungsservice@meinhausarzt.biz</td></tr>
-                                <tr><th>Terminanfrage:</th><td>meintermin@meinhausarzt.biz</td></tr>
-                            </table>
+                            <a href="tel:0711%20345101"><p><span>Telefon:</span> 0711 3451011</p></a>
+                            <p><span>Fax:</span> 0711 52853896</p>
+                            <a href="mailto:dr.schoenthal@meinhausarzt.biz" target="_self"><p><span>E-Mail:</span> dr.schoenthal@meinhausarzt.biz</p></a>
+                            <a href="mailto:rezeptservice@meinhausarzt.biz" target="_self"><p><span>Rezeptbestellung:</span> rezeptservice@meinhausarzt.biz</p></a>
+                            <a href="ueberweisungsservice@meinhausarzt.biz" target="_self"><p><span>Überweisungsanfrage:</span> ueberweisungsservice@meinhausarzt.biz</p></a>
+                            <a href="meintermin@meinhausarzt.biz" target="_self"><p><span>Terminanfrage:</span> meintermin@meinhausarzt.biz</p></a>
                         </div>
                     </div>
                 </div>
